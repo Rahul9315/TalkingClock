3696d478b60681286911b57e9ecac164d489309a
Rahul9315 <baghel931515@gmail.com>
2024-02-14 20:56:01 +0000

    First commit

---
+++ app/src/main/java/com/example/talkingclock/MainActivity.java
@@ -0,0 +1,41 @@
+package com.example.talkingclock;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.os.Bundle;
+
+import com.example.talkingclock.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+    ActivityMainBinding binding;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        replaceFragment(new Alarm_Fragment());
+        binding.bottomNavigationView.setBackground(null);
+        binding.bottomNavigationView.setOnItemSelectedListener(item -> {
+            if (item.getItemId() == R.id.navAlarmlist) {
+                replaceFragment(new Alarm_Fragment());
+            } else if (item.getItemId() == R.id.navStopwatch) {
+                replaceFragment(new Stopwatch_Fragment());
+            } else if (item.getItemId() == R.id.navTimer) {
+                replaceFragment(new Timer_Fragment());
+            } else if (item.getItemId() == R.id.navSetting) {
+                replaceFragment(new Setting_Fragment());
+            }
+            return true;
+        });
+    }
+
+    private void replaceFragment(Fragment fragment) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frame_layout, fragment);
+        fragmentTransaction.commit();
+    }
+}
